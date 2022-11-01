0be83c61b0fb278c8849666f5d2787f1219da632
measser <measserhussien35@gmail.com>
2022-11-01 22:32:08 +0200

    new localization

--- lib/late.dart
+++ lib/late.dart
@@ -1,5 +1,7 @@
+import 'package:education_system/hadana.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'colors.dart';
 import 'componants.dart';
@@ -33,11 +35,18 @@ class LateScreen extends StatelessWidget {
                     top: Radius.circular(25),
                   )),
               child: const TabBar(
+                labelStyle:TextStyle(
+                    fontSize: 18 ,
+                ),
                 automaticIndicatorColorAdjustment: false,
                 indicatorColor: AppColors.zzz,
                 tabs: <Widget>[
                   Tab(
-                    text: 'في انتظار الموافقة',
+                    text: 'في انتظارالموافقة',
+                    // child: Text(
+                    //   'في انتظار الموافقة',
+                    //   style: TextStyle(fontSize: 14,fontWeight: FontWeight.bold),
+                    // ),
                   ),
                   Tab(
                     text: 'تمت الموافقة',
@@ -49,60 +58,191 @@ class LateScreen extends StatelessWidget {
               ),
             ),
           ),
-          title: const Text(
-            'الإستئذانات',
-            style: TextStyle(
+          title:  Text(
+            AppLocalizations.of(context).doctor_masr,
+            style: const TextStyle(
               color: Colors.white,
             ),
           ), //
         ),
         body: Container(
-          decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(
+          decoration:  BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius:const BorderRadius.vertical(
                 top: Radius.circular(25),
               )),
           child: TabBarView(
             children: <Widget>[
-              Column(
-                children:  [
-                  Container(
-                    height: 80,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Colors.grey
-                    ),
-                    child: Row(
-                      children: [
-                        Column(
-                          children: const[
-                            Text(
-                              ' بسام المحترم مصمم التطبيق',
-                              style: TextStyle(
-                                fontSize: 18.0,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: const [
-                  Text('second tab body'),
-                ],
-              ),
-              Column(
-                children: const [
-                  Text('third tab body'),
-                ],
-              ),
+              listOneScreen(),
+              listTwoScreen(),
+              listThreeScreen(),
             ],
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+          child: const Icon(Icons.add,color: Colors.white,size: 45,)
         ),
       ),
     );
   }
 }
+
+Widget listOneScreen()=>ListView.builder(
+  itemBuilder: (context,index)=>Padding(
+    padding: const EdgeInsets.only(top: 8,left: 8,right: 8),
+    child: Column(
+      children:  [
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.white
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 330,
+                      child: const Text(
+                        '  بسام المحترم مصمم التطبيق',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children:  const[
+                        Icon(Icons.date_range_outlined,size: 18,color: Colors.grey,),
+                        SizedBox(width: 3,),
+                        Text('20/02/2022',style: TextStyle(fontSize: 16,color: Colors.grey),),
+                      ],
+                    )
+                  ],
+                ),
+                const Spacer(),
+                const Icon(Icons.arrow_forward_ios_rounded),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  ),
+  itemCount: 10,
+);
+Widget listTwoScreen()=>ListView.builder(
+  itemBuilder: (context,index)=>Padding(
+    padding: const EdgeInsets.only(top: 8,left: 8,right: 8),
+    child: Column(
+      children:  [
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.white
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 330,
+                      child: const Text(
+                        '  بسام المحترم مصمم التطبيق',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children:  const[
+                        Icon(Icons.date_range_outlined,size: 18,color: Colors.grey,),
+                        SizedBox(width: 3,),
+                        Text('20/02/2022',style: TextStyle(fontSize: 16,color: Colors.grey),),
+                      ],
+                    )
+                  ],
+                ),
+                const Spacer(),
+                const Icon(Icons.arrow_circle_right_rounded,color: Colors.green,),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  ),
+  itemCount: 10,
+);
+Widget listThreeScreen()=>ListView.builder(
+  itemBuilder: (context,index)=>Padding(
+    padding: const EdgeInsets.only(top: 8,left: 8,right: 8),
+    child: Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.white
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                 const SizedBox(
+                  child:  Text(
+                    '  بسام المحترم مصمم التطبيق',
+                    style: TextStyle(
+                      fontSize: 16.0,
+
+                    ),
+                  ),
+                ),
+                Row(
+                  children:  const[
+                    Icon(Icons.date_range_outlined,size: 18,color: Colors.grey,),
+                    SizedBox(width: 3,),
+                    Text('20/02/2022',style: TextStyle(fontSize: 16,color: Colors.grey),),
+                  ],
+                )
+              ],
+            ),
+            const Spacer(),
+            InkWell(
+              onTap: (){
+                //navegateTo(context, null);
+              },
+              child: Container(
+                width:102,
+                height: 45,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.red,
+                ),
+                child:const  Center(
+                  child:  Text(
+                    'لماذا الرفض؟',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    ),
+  ),
+  itemCount: 10,
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 import 'colors.dart';
 import 'late.dart';
@@ -14,6 +16,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      locale: Locale('en',''),
+      builder: (context, child) => ResponsiveWrapper.builder(
+        child,
+        maxWidth: 1200,
+        minWidth: 480,
+        defaultScale: true,
+        breakpoints: const [
+          ResponsiveBreakpoint.autoScale(425, name: MOBILE),
+          ResponsiveBreakpoint.autoScale(800, name: TABLET),
+          ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+        ],
+      ),
       theme: ThemeData(
         fontFamily: 'Poppins',
         listTileTheme: const ListTileThemeData(
